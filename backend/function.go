4ef5069ddb6a2494ab70965771665055183ef13e
Alexey Eremin <a.eremin.msu@gmail.com>
2024-10-26 12:59:46 +0200

    Add basic Firestore interaction to (hopefully) being able to serve redirects

--- backend/function.go
+++ backend/function.go
@@ -1,14 +1,26 @@
 package helloworld
 
 import (
+	"cloud.google.com/go/firestore"
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
 
+var client *firestore.Client
+var ctx = context.Background()
+
 func init() {
+	var err error
+	client, err = firestore.NewClient(ctx, "alice-larp")
+	if err != nil {
+		log.Fatalf("Failed to create Firestore client: %v", err)
+	}
+
 	functions.HTTP("API", api)
 }
 
@@ -16,9 +28,12 @@ type body struct {
 	Id string `json:"id"`
 }
 
+type UrlDocument struct {
+	Url string `firestore:"url"`
+}
+
 func api(w http.ResponseWriter, r *http.Request) {
 	mux.ServeHTTP(w, r)
-	return
 }
 
 func create(w http.ResponseWriter, r *http.Request) {
@@ -35,6 +50,18 @@ func redirect(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Shit is broken!")
 		return
 	}
+
+	d, err := client.Collection("urls").Doc(b.Id).Get(ctx)
+	if err != nil {
+		fmt.Fprint(w, "Shit is broken 2!")
+		return
+	}
+	var doc UrlDocument
+	if err := d.DataTo(&doc); err != nil {
+		fmt.Fprint(w, "Shit is broken 3!")
+	}
+
+	http.Redirect(w, r, doc.Url, 301)
 }
 
 var mux = newMux()
--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module example.com/gcf
 
 go 1.22.2
 
-require github.com/GoogleCloudPlatform/functions-framework-go v1.5.2
+require (
+	cloud.google.com/go/firestore v1.17.0
+	github.com/GoogleCloudPlatform/functions-framework-go v1.5.2
+)
